3cd8dea598ba939cbb7285e6c8a44ad949255dbf
Vishnu M <vishnu240404@gmail.com>
2024-10-17 20:23:03 +0530

    Might be challenging

---
+++ Day-1/issue4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void del(int arr, int element) {
+    // Define a function to delelte an element from the array passed as argument.
+}
+
+
+int main() {
+    int arr[5] = {2, 3, 4, 5, 7};
+
+    del(arr, 5);
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
